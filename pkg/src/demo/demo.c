a46882a42bcfc9dee2890ae4671b37e38632e14e
Clif Liu <clif@afero.io>
2017-11-07 13:33:17 -0800

    Update to latest edge API

--- pkg/src/demo/demo.c
+++ pkg/src/demo/demo.c
@@ -34,10 +34,10 @@ static char g_dev_serNum[] = "1.0.0";
 static char g_plat_verNum[] = "1.0.0";
 static char g_hub_verNum[] = "1.0.0";
 
-static bool set_handler(const uint8_t request_id, const uint16_t attr_id, const uint16_t value_len, const uint8_t *value) {
+static bool set_handler(const uint16_t attr_id, const uint16_t value_len, const uint8_t *value) {
     char hex_buffer[1024];
     af_util_buffer_to_hex(hex_buffer, sizeof(hex_buffer), value, value_len);
-    AFLOG_INFO("EDGE DEMO: SET (request_id=%u): %i = 0x%s", request_id, attr_id, hex_buffer);
+    AFLOG_INFO("EDGE DEMO: SET: %i = 0x%s", attr_id, hex_buffer);
 
     // allow updates to any attribute except the one we're demo'ing.
     if (attr_id != ATTR_SIREN_ENABLED) return true;
@@ -52,19 +52,22 @@ static bool set_handler(const uint8_t request_id, const uint16_t attr_id, const 
     return true;
 }
 
-static void notify_handler(const uint8_t request_id, const uint16_t attr_id, const uint16_t value_len, const uint8_t *value) {
+static void notify_handler(const uint16_t attr_id, const uint16_t value_len, const uint8_t *value) {
     char hex_buffer[1024];
     af_util_buffer_to_hex(hex_buffer, sizeof(hex_buffer), value, value_len);
-    AFLOG_INFO("EDGE DEMO: NOTIFY (request_id=%u): %i = 0x%s", request_id, attr_id, hex_buffer);
+    AFLOG_INFO("EDGE DEMO: NOTIFY: %i = 0x%s", attr_id, hex_buffer);
 }
 
 static void update_battery(evutil_socket_t fd, short events, void *arg) {
-    AFLOG_INFO("EDGE DEMO: decrement battery level!");
-    g_battery--;
-    // drain twice as fast if the siren is enabled.
-    if (g_siren_enabled) g_battery--;
-    if (g_battery < 0) g_battery += 100;
-    aflib_set_attribute_i8(ATTR_BATTERY_LEVEL, g_battery);
+    // only drain if siren is enabled
+    if (g_siren_enabled) {
+        AFLOG_INFO("EDGE DEMO: decrement battery level!");
+        g_battery--;
+        if (g_battery < 0) {
+            g_battery += 100;
+        }
+        aflib_set_attribute_i8(ATTR_BATTERY_LEVEL, g_battery);
+    }
 }
 
 int main(int argc, char **argv) {
@@ -92,7 +95,7 @@ int main(int argc, char **argv) {
 
     /* set a recurring timer for 15 seconds */
     struct event *timer = event_new(ev, -1, EV_TIMEOUT | EV_PERSIST, update_battery, NULL);
-    struct timeval period = { .tv_sec = 10, .tv_usec = 0 };
+    struct timeval period = { .tv_sec = 15, .tv_usec = 0 };
     event_add(timer, &period);
 
     event_base_dispatch(ev);
